cf325b6187fbeeecf95bdf6b1ec27b0b18d605ca
trian-gles <69212477+trian-gles@users.noreply.github.com>
2021-02-28 12:03:34 -0500

    Finished fretbox form layout

--- webpage/static/render_boxes.js
+++ webpage/static/render_boxes.js
@@ -16,20 +16,28 @@ function Box(props) {
   let num = props.num
   return (rows.map(row => (
   <div key={row[0]}>
+    <input type="text" class="fret_label" name={[num, row[0]]}></input>
     {row.map(item => (
     <input type="checkbox" name={[num].concat(item)} key={item} id="fret"></input>))}
   </div>)))
 }
 
-const BOXES = [0, 1, 2, 3, 4, 5].map(num => (
-  <div class="fretbox"><Box num={num}/></div>
+const BOXES = [0, 1, 2, 3, 4, 5, 6, 7, 8].map(num => (
+  <div class="fretbox">
+    <input type="text" class="box_title" name={num}></input>
+    <Box num={num}/>
+  </div>
 ));
 
 var App = function() {
   return (
     <form action='/' method="POST">
-      {BOXES}
-      <input type="submit"></input>
+        <div id="all_boxes">
+          {BOXES}
+        </div>
+      <div id="submit-div">
+        <input type="submit"></input>
+      </div>
     </form>
   );
 };
